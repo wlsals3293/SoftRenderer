75e05ff953ff31e33cfeb1ea1b3f493e5defcaec
wlsals3293 <31675836+wlsals3293@users.noreply.github.com>
2019-08-26 14:28:48 +0900

    Add files via upload

---
+++ CMake-VS-15-2017-Win64.bat
@@ -0,0 +1,18 @@
+@echo off
+
+@echo Generating visual studio 2017 64Bit solutions
+PUSHD %~dp0
+
+@echo Check exist Project folder
+IF EXIST "Project" rd "Project" /s /q
+@echo Make new Project folder
+md Project
+cd Project
+@echo Run CMake Script
+cmake -DBUILD_PLAYER=1 -G "Visual Studio 15 2017 Win64" ..\
+
+@echo:
+@echo Solution created in %~dp0\Project
+@echo:
+POPD
+PAUSE
