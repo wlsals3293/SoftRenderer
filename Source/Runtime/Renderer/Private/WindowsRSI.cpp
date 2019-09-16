f557cc8de8ec78869490c765a72c083ca152b0c7
wlsals3293 <31675836+wlsals3293@users.noreply.github.com>
2019-09-16 12:42:07 +0900

    Add files via upload

--- Source/Runtime/Renderer/Private/WindowsRSI.cpp
+++ Source/Runtime/Renderer/Private/WindowsRSI.cpp
@@ -24,3 +24,46 @@ void WindowsRSI::BeginFrame()
 {
 
 }
+
+void WindowsRSI::DrawTriangle(const Vector2& P1, const Vector2& P2, const Vector2& P3)
+{
+	Vector2 minPos = Vector2::Zero;
+	Vector2 maxPos = Vector2::Zero;
+
+	minPos.X = Math::Min(P1.X, P2.X);
+	minPos.X = Math::Min(minPos.X, P3.X);
+	minPos.Y = Math::Min(P1.Y, P2.Y);
+	minPos.Y = Math::Min(minPos.Y, P3.Y);
+
+	maxPos.X = Math::Max(P1.X, P2.X);
+	maxPos.X = Math::Max(maxPos.X, P3.X);
+	maxPos.Y = Math::Max(P1.Y, P2.Y);
+	maxPos.Y = Math::Max(maxPos.Y, P3.Y);
+
+	Vector2 u = P2 - P1;
+	Vector2 v = P3 - P1;
+	float dotUU = u.Dot(u);
+	float dotUV = u.Dot(v);
+	float dotVV = v.Dot(v);
+	float invDenom = 1.0f / (dotUU * dotVV - dotUV * dotUV);
+
+	ScreenPoint minPoint(Math::TruncToInt(minPos.X), Math::TruncToInt(minPos.Y));
+	ScreenPoint maxPoint(Math::TruncToInt(maxPos.X), Math::TruncToInt(maxPos.Y));
+
+	for (int x = minPoint.X; x < maxPoint.X; x++)
+	{
+		for (int y = minPoint.Y; y < maxPoint.Y; y++)
+		{
+			ScreenPoint point(x, y);
+			Vector2 w = Vector2(point.X, point.Y) - P1;
+			float dotUW = u.Dot(w);
+			float dotVW = v.Dot(w);
+			float s = (dotVV * dotUW - dotUV * dotVW) * invDenom;
+			float t = (dotUU * dotVW - dotUV * dotUW) * invDenom;
+			if (s >= 0 && t >= 0 && ((s + t) <= 1))
+			{
+				PutPixel(point);
+			}
+		}
+	}
+}
