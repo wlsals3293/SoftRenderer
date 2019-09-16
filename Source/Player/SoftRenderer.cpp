c237e5e5f111fc36a385753ae968ede371132cf3
wlsals3293 <31675836+wlsals3293@users.noreply.github.com>
2019-09-16 12:40:56 +0900

    Add files via upload

--- Source/Player/SoftRenderer.cpp
+++ Source/Player/SoftRenderer.cpp
@@ -27,6 +27,14 @@ void SoftRenderer::Update()
 	if (RSI != nullptr)
 	{
 		RSI->Clear(LinearColor(0.125f, 0.5f, 1.f, 1.f));
+
+		Vector2 P1(70.f, 150.f);
+		Vector2 P2(-150.f, 0.f);
+		Vector2 P3(150.f, 0.f);
+
+		RSI->SetColor(LinearColor(1.f, 0.f, 0.f, 1.f));
+		RSI->DrawTriangle(P1, P2, P3);
+
 		RSI->EndFrame();
 	}
 }
